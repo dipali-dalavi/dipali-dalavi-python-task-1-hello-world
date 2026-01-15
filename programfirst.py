05d4693310cca87513f0cad7a12f09b9ff3a26d9
Dipali_Dalavi <167181167+dipali-dalavi@users.noreply.github.com>
2026-01-15 16:45:36 +0530

    Add files via upload

---
+++ programfirst.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+name = "Dipali Sambhaji Dalavi"
+role = "Python Intern"
+today_date = date.today()
+
+print("Name:", name)
+print("Internship Role:", role)
+print("Date:", today_date)
